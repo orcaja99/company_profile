1a6d558f17ce4cbaa17d85d4837e9396fe7f70fb
orcaja99 <tealatte8@gmail.com>
2023-07-17 13:45:32 +0800

    perbaruin

--- resources/views/berita/show.blade.php
+++ resources/views/berita/show.blade.php
@@ -1,25 +1,38 @@
-<!-- resources/views/berita/show.blade.php -->
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Detail berita</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Detail berita</h1>
+    <div class="container mt-4">
+        <h1>Detail berita</h1>
 
-    <p>ID: {{ $berita->id }}</p>
-    <p>Judul: {{ $berita->judul }}</p>
-    <p>Gambar:
-        @if($berita->gambar)
-        <img src="{{ asset('storage/images/' . $berita->gambar) }}" alt="Gambar berita" width="100">
-        @else
-        Tidak ada gambar
-        @endif
-    </p>
-    <p>Tanggal: {{ $berita->tanggal }}</p>
-    <p>Keterangan: {{ $berita->keterangan }}</p>
+        <div class="row">
+            <div class="col-md-6">
+                <p><strong>ID:</strong> {{ $berita->id }}</p>
+                <p><strong>Judul:</strong> {{ $berita->judul }}</p>
+                <p>
+                    <strong>Gambar:</strong>
+                    @if($berita->gambar)
+                    <br>
+                    <img src="{{ asset('storage/images/' . $berita->gambar) }}" alt="Gambar berita">
+                    @else
+                    Tidak ada gambar
+                    @endif
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p><strong>Tanggal:</strong> {{ $berita->tanggal }}</p>
+                <p><strong>Keterangan:</strong> {{ $berita->keterangan }}</p>
+            </div>
+        </div>
 
-    <a href="{{ route('berita.index') }}">Kembali</a>
+        <a href="{{ route('berita.index') }}" class="btn btn-primary mt-3">Kembali</a>
+    </div>
+
+    <!-- Include Bootstrap JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
